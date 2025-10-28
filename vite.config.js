fef95bb9b8c31813eb424b50c0f236caf2b8e2dd
Ritesh Kumar Singh <riteshme0090@gmail.com>
2025-10-28 17:43:55 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/CurrencyXchange-Cithub-Action/',
+  base: '/CurrencyXchange-Cithub-Action/', // 👈 add this line
 })
+
