51420731283c996873d71a6ff68e6620b9c7df55
Ritesh Kumar Singh <riteshme0090@gmail.com>
2025-10-28 18:31:20 +0530

    Fix Vite base path and deploy config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/CurrencyXchange-Cithub-Action/', // 👈 add this line
+  base: '/CurrencyXchange-Cithub-Action/', // 👈 must exactly match your repo name
 })
 
